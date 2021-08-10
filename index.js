3e903e317c172de75f872e16722a04a603be880a
Ryan Guthrie <ryanguthrie@zoho.com>
2021-08-10 11:30:37 +0100

    commit tree db7412bae7fd82d1ae60d3f0bb0a3b5b95d4475e

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2
+const num2 = 31
 
-// Write your code here
+let multiply = num1*num2
+
+let random = Math.floor(Math.random()*10+1)
+
+const num3 = 24
+const num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(5,10,15,20)
